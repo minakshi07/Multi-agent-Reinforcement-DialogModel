b34d42538d59217469111f029ae424daeb1a91bc
Minakshi Kudalkar <42650284+minakshi07@users.noreply.github.com>
2020-03-18 23:12:01 +0530

    Primary bot 1 with intents

---
+++ main.py
@@ -0,0 +1,28 @@
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy
+import tflearn
+import tensorflow
+import random
+
+import json
+with open('intents.json') as file:
+    data = json.load(file)
+
+
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data['intents']:
+    for pattern in intent['patterns']:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs_x.append(wrds)
+        docs_y.append(intent["tag"])
+        
+    if intent['tag'] not in labels:
+        labels.append(intent['tag'])
