04f8582ca6df5213e27579e9c5712497efa8a943
Minakshi Kudalkar <42650284+minakshi07@users.noreply.github.com>
2020-03-19 13:39:37 +0530

    data preprocessing and handling import module bugs

--- main.py
+++ main.py
@@ -6,23 +6,56 @@ import numpy
 import tflearn
 import tensorflow
 import random
-
 import json
-with open('intents.json') as file:
+
+
+with open("intents.json") as file:
     data = json.load(file)
 
+print("hey im working!")
 
 words = []
 labels = []
 docs_x = []
 docs_y = []
 
-for intent in data['intents']:
-    for pattern in intent['patterns']:
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
         docs_x.append(wrds)
         docs_y.append(intent["tag"])
-        
-    if intent['tag'] not in labels:
-        labels.append(intent['tag'])
+
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
+
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x):
+    bag = []
+
+    wrds = [stemmer.stem(w.lower()) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+
+training = numpy.array(training)
+output = numpy.array(output)
